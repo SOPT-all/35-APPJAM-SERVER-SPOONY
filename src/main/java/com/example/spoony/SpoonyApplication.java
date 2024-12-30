6e5054c5db48a8fac5e64d865bd75634f3ccbac7
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2024-12-30 15:55:23 +0900

    [feat] 초기 프로젝트 설정

---
+++ src/main/java/com/example/spoony/SpoonyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spoony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spoony/SpoonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
